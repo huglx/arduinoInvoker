b5f23a838fd713ff2fb23101996c7bc7a8fe8ba2
Manzhosov <manzhiva@cvut.cz>
2025-04-07 16:08:23 +0200

    a

--- src/InvokerTrainer/sound.cpp
+++ src/InvokerTrainer/sound.cpp
@@ -16,23 +16,23 @@ void Sound::playWrong() {
 }
 
 void Sound::playStart() {
-    playTone(800, 200);
-    delay(100);
-    playTone(1000, 200);
-    delay(100);
-    playTone(1200, 200);
+    // playTone(800, 200);
+    // delay(100);
+    // playTone(1000, 200);
+    // delay(100);
+    // playTone(1200, 200);
 }
 
 void Sound::playEnd() {
-    playTone(1200, 200);
-    delay(100);
-    playTone(1000, 200);
-    delay(100);
-    playTone(800, 200);
+    // playTone(1200, 200);
+    // delay(100);
+    // playTone(1000, 200);
+    // delay(100);
+    // playTone(800, 200);
 }
 
 void Sound::playTone(int frequency, int duration) {
-    Esplora.tone(frequency, duration);
-    delay(duration);
-    Esplora.noTone();
+    // Esplora.tone(frequency, duration);
+    // delay(duration);
+    // Esplora.noTone();
 } 
