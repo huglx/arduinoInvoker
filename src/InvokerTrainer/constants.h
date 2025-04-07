469a8e0b269929b2f822bb81439e280cd2ee8ad7
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2025-04-07 15:44:35 +0200

    fix errors

--- src/InvokerTrainer/constants.h
+++ src/InvokerTrainer/constants.h
@@ -12,32 +12,10 @@
 #define SPELL_LENGTH 3
 
 // Spell names
-const char* SPELL_NAMES[MAX_SPELLS] = {
-    "Cold Snap",        // QQQ
-    "Ghost Walk",       // QQW
-    "Ice Wall",         // QQE
-    "EMP",             // WWW
-    "Tornado",         // WWQ
-    "Alacrity",        // WWE
-    "Sun Strike",      // EEE
-    "Forge Spirit",    // EEQ
-    "Chaos Meteor",    // EEW
-    "Deafening Blast"  // QWE
-};
+extern const char* SPELL_NAMES[MAX_SPELLS];
 
 // Spell combinations (Q=1, W=2, E=3)
-const int SPELL_COMBINATIONS[MAX_SPELLS][SPELL_LENGTH] = {
-    {1, 1, 1},  // Cold Snap
-    {1, 1, 2},  // Ghost Walk
-    {1, 1, 3},  // Ice Wall
-    {2, 2, 2},  // EMP
-    {2, 2, 1},  // Tornado
-    {2, 2, 3},  // Alacrity
-    {3, 3, 3},  // Sun Strike
-    {3, 3, 1},  // Forge Spirit
-    {3, 3, 2},  // Chaos Meteor
-    {1, 2, 3}   // Deafening Blast
-};
+extern const int SPELL_COMBINATIONS[MAX_SPELLS][SPELL_LENGTH];
 
 // Game modes
 enum GameMode {
